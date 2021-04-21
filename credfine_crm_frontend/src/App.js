1d0310545b75bb0c1d2e69baf11aefab9740dbd3
sourav1994 <sourav@credfine.com>
2021-04-21 17:59:48 +0530

    createuser

--- credfine_crm_frontend/src/App.js
+++ credfine_crm_frontend/src/App.js
@@ -1,12 +1,28 @@
 import Login from "./containers/Login/Login";
 import "bootstrap/dist/css/bootstrap.min.css";
 import SimpleHeader from "./includes/SimpleHeader";
-
+import UserCreate from "./containers/Login/UserCreate/UserCreate";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 function App() {
   return (
-    <div>
-      <Login />
+    <Router>
+      <div>
+      <Switch>
+        <Route exact path="/">
+          <Login/>
+        </Route>
+        <Route path="/userCreate">
+          <UserCreate/>
+        </Route>
+      </Switch>
     </div>
+    </Router>
+    
   );
 }
 
--- credfine_crm_frontend/src/containers/Login/Login.js
+++ credfine_crm_frontend/src/containers/Login/Login.js
@@ -1,6 +1,7 @@
 import React,{useState} from 'react'
 import "./Login.css";
 import {Form, Card , Button } from 'react-bootstrap';
+import {Link} from "react-router-dom"
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -10,14 +11,15 @@ export default function Login() {
   }
   function submitHandler(event){
     event.preventDefault();
+    
   }
   return (
     <div className="Login">
       <Card className="Card">
       <Form onSubmit={submitHandler}>
-      <Form.Label>Login</Form.Label>
+      <Form.Label style={{fontWeight:"bold"}}>Login</Form.Label>
       <Form.Group size="lg" controlId="email">
-      <Form.Label>Email</Form.Label>
+      <Form.Label>Email Id / Emp Code</Form.Label>
       <Form.Control 
       autoFocus
       type="email"
@@ -27,12 +29,13 @@ export default function Login() {
       <Form.Group size="lg" controlId="password">
       <Form.Label>Password</Form.Label>
       <Form.Control 
-      
+
       type="password"
       value={password}
       onChange={(e)=> setPassword(e.target.value)}/>
       </Form.Group>
      <Button variant="success" block size="md" type="submit" disabled={!validateForm}>LOGIN</Button>
+     <Link to="/userCreate">Create User</Link>
       </Form>
       </Card>
     </div>
