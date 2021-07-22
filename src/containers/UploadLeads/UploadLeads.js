15bae0f6b332491fd91bb424e0b19c8d47e4359b
sourav1994 <sourav@credfine.com>
2021-07-22 18:01:36 +0530

    modify bulk_upload

--- src/containers/UploadLeads/UploadLeads.js
+++ src/containers/UploadLeads/UploadLeads.js
@@ -4,60 +4,45 @@ import baseUrl from '../../global/api';
 import style from './UploadLeads.module.css';
 import { Form, Card, Button, Row, Col ,Alert} from "react-bootstrap";
 import {getProfileData} from '../../global/leadsGlobalData'
-// import * as ReactBootstrap from "react-bootstrap";
 import SampleData from './SampleData';
 export default function UploadLeads() {
   const profileData = getProfileData();
   const [loanType,setLoanType] = useState('');
-  const [errors,setErrors] = useState({});
   const [validated, setValidated] = useState(false);
   const [file,setFile] = useState('');
   const [alertMessage,setAlertMessage] = useState('');
   const [isDisplay,setIsDisplay] = useState(false);
   const leadsSubmitHandler =async (event)=>{
     const form = event.currentTarget;
-    if (form.checkValidity() === false) {
+    if (form.checkValidity() === true) {
       event.preventDefault();
       event.stopPropagation();
+      const fromData= new FormData();
+      fromData.append('file',file);
+      let headers = {
+        'Authorization': `Token ${profileData.token}`,
+        'Content-Type': 'multipart/form-data'
+      }
+      
+      await axios.post(`${baseUrl}/leads/lead_bulk_upload/PL`,fromData,{headers})
+      .then((response)=>{
+        setAlertMessage(`${response.data.created_count} Leads Successfully uploaded and
+         ${response.data.duplicated_count} Was Duplicated`)
+        setIsDisplay(true);
+      }).catch(error =>{
+        setAlertMessage('wrong File Was choosen')
+        setIsDisplay(true);
+      })
+    }else{
+      setValidated(true);
+      event.preventDefault();
     }
-    setValidated(true);
-    event.preventDefault();
-    
-    const fromData= new FormData();
-    fromData.append('file',file);
-    let headers = {
-      'Authorization': `Token ${profileData.token}`,
-      'Content-Type': 'multipart/form-data'
-    }
-    
-     await axios.post(`${baseUrl}/leads/lead_bulk_upload/PL`,fromData,{headers})
-     .then((response)=>{
-       setAlertMessage(`${response.data.count} Leads Successfully uploaded`)
-       setIsDisplay(true);
-     }).catch(error =>{
-       setAlertMessage('wrong File Was choosen')
-       setIsDisplay(true);
-     })
-       
-     
-    // const newErrors = findErrors();
-    // setErrors(newErrors);
-    
-
   }
-  const findErrors = ()=>{
-    const newErrors = {};
-    if(!loanType || loanType === ''){
-      newErrors.loanType = 'This is required field'
-    }
-    return newErrors;
-    
-}
-    return (
+   return (
       <div >
         <Form noValidate validated={validated} onSubmit={leadsSubmitHandler}>
           <Card className={style.Card}>
-          {isDisplay ? <Alert variant="primary">{alertMessage}</Alert> : null}
+          {isDisplay && <Alert className={style.alertBox}>{alertMessage}</Alert>}
             <Form.Group>
               <Form.Label>Loan Type</Form.Label>
               <Form.Control required as="select" 
