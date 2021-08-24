bb99e80c65e5bb398db519ff2062d11a717664bc
sourav1994 <sourav@credfine.com>
2021-08-24 16:40:46 +0530

    change vertage api http to https

--- src/global/callApi.js
+++ src/global/callApi.js
@@ -1,2 +1,2 @@
 export const clickToCallApi = "https://api-smartflo.tatateleservices.com/v1/click_to_call_support";
-export const vertageDialerApi = "http://192.168.0.100/agent/api.php?source=api";
+export const vertageDialerApi = "https://192.168.0.100/agent/api.php?source=api";
