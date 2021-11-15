cf7d8d3b34638c01857d5868153488a9448abda7
sourav1994 <sourav@credfine.com>
2021-11-15 12:46:10 +0530

    dialer api changes http to https

--- src/global/callApi.js
+++ src/global/callApi.js
@@ -1,3 +1,3 @@
 export const clickToCallApi = "https://api-smartflo.tatateleservices.com/v1/click_to_call_support";
-export const haloocomDialerApi = "http://192.168.0.85/HaloocomAPIS";
+export const haloocomDialerApi = "https://192.168.0.85/HaloocomAPIS";
 
