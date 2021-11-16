1afc0beba1570c8066c602371c61ab7e5c54b7df
sourav1994 <sourav@credfine.com>
2021-11-16 19:21:30 +0530

    modify in code of public ip with 2 locations

--- src/global/callApi.js
+++ src/global/callApi.js
@@ -1,8 +1,4 @@
 export const clickToCallApi = "https://api-smartflo.tatateleservices.com/v1/click_to_call_support";
-<<<<<<< HEAD
 export const haloocomNoidaDialerApi = "https://103.122.169.10/HaloocomAPIS";
 export const haloocomMumbaiDialerApi = "https://103.158.49.198/HaloocomAPIS";
-=======
-export const haloocomDialerApi = "http://103.122.169.10//HaloocomAPIS";
->>>>>>> c82e6653b0be1c230abc30de848787f13bf3ab0e
 
