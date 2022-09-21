5d4d2e3271b0704f82c92dc09b95d50a80774e26
sourav1994 <sourav@credfine.com>
2022-09-21 16:39:48 +0530

    dialer SSO api issue solve

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -22,7 +22,6 @@ export default function Dashboard() {
         <PageLayerSection isDisplaySearchBar={false} ActualEmiCalculate={openCalculator} ActualEligibilityCalculate={openEligibility}>
             <EligibilityCalculator isOpenEligibilityCalculator={checkEligibility} isCloseEligibilityCalculator={closeEligibility} />
             <EmiCalculator isOpenCalculator={openCalculate} isCloseCalculator={closeCalculator} />
-            <iframe src="https://credfine.slashrtc.in/index.php/ssoLogin?crmUniqueId=CeuCxaZUVpk+Stxmt5qIWA==&usernameId=sourav1994&requestOrigin=http://crm.credfine.com/" width="300px" height="300px" frameBorder="0" title="description"></iframe>
             <NoDataFound text="No Data Found" />
         </PageLayerSection>
     )
--- src/containers/Dialer/Dialer.js
+++ src/containers/Dialer/Dialer.js
@@ -3,18 +3,14 @@ import axios from 'axios'
 import PageLayerSection from '../PageLayerSection/PageLayerSection'
 
 export default function Dialer() {
-    const loginHandler = async () => {
-        await axios.post('https://credfine.slashrtc.in/index.php/ssoLogin?crmUniqueId=CeuCxaZUVpk+Stxmt5qIWA==&usernameId=sourav1994&requestOrigin=http://localhost:3000/')
-        .then((response)=>{
-            console.log("working")
-        }).catch((error)=>{
-            console.log("error")
-        })
-    }
     return (
         <PageLayerSection isDisplaySearchBar={true} >
-            {/* <iframe src='https://credfine.slashrtc.in/index.php/ssoLogin?crmUniqueId=CeuCxaZUVpk+Stxmt5qIWA==&usernameId=sourav1994&requestOrigin=http://localhost:3000/' width='100%' height='540' frameBorder='0'></iframe> */}
-            <button onClick={loginHandler}>login</button>
+            <iframe src='https://credfine.slashrtc.in/index.php/ssoLogin?crmUniqueId=CeuCxaZUVpk+Stxmt5qIWA==&usernameId=sourav1994&requestOrigin=http://crm.credfine.com/' 
+            width='100%' 
+            height='540' 
+            frameBorder='0'
+            allow='camera; microphone'
+            ></iframe>
         </PageLayerSection>
     )
 }
